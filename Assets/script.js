518f87adfafe47dbe0127d064eb41a8f80013caf
Sammy Sloan <smileysammy3@gmail.com>
2020-04-26 13:24:58 -0600

    updated the java to shorten the loading data in

--- Assets/script.js
+++ Assets/script.js
@@ -11,15 +11,11 @@ $(document).ready(function () {
 
     // Load in the saved data from the local storage
 
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $(".time-block").each(function () {
+        var hour = $(this).attr("id");
+        var text = localStorage.getItem(hour);
+        $(this).find("textarea").val(text);
+    });
 
     function hourColor() {
         // Create a variable for the current hour
